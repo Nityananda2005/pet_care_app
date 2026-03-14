a8b73a895b8ac301e8d332ed9498adea5a1aeb6d
Nityananda Dalei <nityanandadalei2005@gmail.com>
2026-03-14 14:02:40 +0530

    Always start server regardless of NODE_ENV

--- backend/server.js
+++ backend/server.js
@@ -56,10 +56,9 @@ connectDB();
 startScheduler();
 
 const PORT = process.env.PORT || 5000;
-if (process.env.NODE_ENV !== 'production') {
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
-}
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 module.exports = app;
